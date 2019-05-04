7228aa80d2aedbf2ebf6445357b6eae63fb8026c
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2019-05-04 23:26:19 +0100

    Updates

---
+++ precache-manifest.9103e08d9c3e3812c21e4d52d06e6357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bcd4d721394cae729d1e22521b54cc",
+    "url": "/JohnOlushola/react-tutorial.git/index.html"
+  },
+  {
+    "revision": "14e2dc8b071dae77c8da",
+    "url": "/JohnOlushola/react-tutorial.git/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/JohnOlushola/react-tutorial.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "14e2dc8b071dae77c8da",
+    "url": "/JohnOlushola/react-tutorial.git/static/js/main.44ea7ca2.chunk.js"
+  },
+  {
+    "revision": "04419d6a6138963a1136",
+    "url": "/JohnOlushola/react-tutorial.git/static/js/runtime~main.3eeb0261.js"
+  }
+]);
